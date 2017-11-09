9c415ee10d67eb49ab55fb5d14b8d683a9ffddd5
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2017-11-09 15:57:49 +0100

    basic test for abstract base models

---
+++ bucketie/tests/models.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+
+from django.test import TestCase
+from django.utils.timezone import now
+
+from bucketie.models import TimeStampedModel, DescribedModel
+
+class TimedModel(TimeStampedModel):
+    pass
+
+class DetailedModel(DescribedModel):
+    pass
+
+class TestTimeStampedModels(TestCase):
+    def test_created_at_field(self):
+        create_start = now()
+        timed = TimedModel()
+        create_end = now()
+        timed.save()
+
+        self.assertGreater(timed.created_at, create_start)
+        self.assertLess(timed.created_at, create_end)

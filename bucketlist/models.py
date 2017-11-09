e9d9d42fdcd27dd37a3cd1748427da659ea38368
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2017-11-09 16:27:52 +0100

    Removed single test file and added test package to the bucketlist django app

--- bucketlist/models.py
+++ bucketlist/models.py
@@ -2,11 +2,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 
 from bucketie.models import TimeStampedModel, DescribedModel
 
 class BucketList(TimeStampedModel, DescribedModel):
+    """
+    BucketList model which will act as a container for BucketList items
+    """
     pass
 
 class BucketListItem(TimeStampedModel, DescribedModel):
-    pass
+    """
+    Model which represents the items that are stored in a BucketList
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    done = models.BooleanField(default=False)
+
+    def mark_done(self):
+        """Mark this BucketList Item as done"""
+        self.done = True
+        self.save()

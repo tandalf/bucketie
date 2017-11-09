16b36fa6665e393500e5413098e79bd07d7e3958
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2017-11-09 22:15:56 +0100

    added BucketListViewSet
    A BucketListViewSet has been added to the bucketlist.views module
    to enable api requests for the /blocklist/ route

--- bucketlist/views.py
+++ bucketlist/views.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from rest_framework import viewsets, generics
 
-# Create your views here.
+from .models import UserModel, BucketList
+from .serializers import BucketListSerializer, RetrieveUserSerializer
+
+class UserView(generics.RetrieveAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = RetrieveUserSerializer
+
+class BucketListViewSet(viewsets.ModelViewSet):
+    """
+    View set created with the responsibility of listing, retrieving or deleting
+    BucketList views
+    """
+    queryset = BucketList.objects.all()
+    serializer_class = BucketListSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)

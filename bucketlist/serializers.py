55806b35d8990be4815538d72c263f92898654c7
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2017-11-09 22:23:57 +0100

    created serializers for the BucketList model

---
+++ bucketlist/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import UserModel, BucketList, BucketListItem
+
+class RetrieveUserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('id', 'url', 'username', 'first_name', 'last_name')
+        read_only_fields = ('id', 'url', 'username', 'first_name', 'last_name')
+
+class BucketListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BucketList
+        fields = ('url', 'owner', 'name', 'description', 'created_at', 'modified_at')
+        read_only_fields = ('url', 'owner', 'created_at', 'modified_at')
+        

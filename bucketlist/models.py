ec6af5e90ee0e4fe779149425d31718e2fcd5342
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2017-11-09 17:34:34 +0100

    added some documentation to the bucketlist app models.py

--- bucketlist/models.py
+++ bucketlist/models.py
@@ -8,6 +8,11 @@ from django.contrib.auth import get_user_model
 from bucketie.models import TimeStampedModel, DescribedModel
 
 def default_user():
+    """
+    Gets the id of the default user that will be used to populate the owner
+    of a bucketlist if none is provided.
+    This function is useful for db migration issues.
+    """
     user_id = -1
     try:
         user_id = get_user_model().objects.get(username='superuser').id
@@ -26,6 +31,9 @@ class BucketList(TimeStampedModel, DescribedModel):
     def add_item(self, item, save=False, *args, **kwargs):
         """
         Convinience method for adding items to a BucketList
+
+        :param save: boolean indicating if this model should be saved immediately
+                        after adding an item to the bucketlist
         """
         self.items.add(item)
         if save:
@@ -40,8 +48,3 @@ class BucketListItem(TimeStampedModel, DescribedModel):
     assigned_user = models.ForeignKey(settings.AUTH_USER_MODEL, 
         default=default_user)
     done = models.BooleanField(default=False)
-
-    def mark_done(self):
-        """Mark this BucketList Item as done"""
-        self.done = True
-        self.save()

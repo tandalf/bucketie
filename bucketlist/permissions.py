4cf6959b99aeff64fedb6ca76fac082616aa6f2c
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2017-11-10 09:56:18 +0100

    added permission class for BucketListItems

--- bucketlist/permissions.py
+++ bucketlist/permissions.py
@@ -5,3 +5,8 @@ class OwnerOnlyPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
+
+class OwnerAndAssignedUserPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.assgined_user == request.user or \
+            obj.bucketlist.owner == request.user

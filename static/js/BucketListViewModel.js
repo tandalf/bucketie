5cf071a8e04949ff62e6006f8b8674ff27ffc64e
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2017-11-11 16:56:05 +0100

    added listing functionality to the BucketListItemViewModel

--- static/js/BucketListViewModel.js
+++ static/js/BucketListViewModel.js
@@ -1,6 +1,8 @@
 var BucketListViewModel = function(createBucketListURL){
     var self = this;
 
+    self.bucketListItems = ko.observableArray();
+
     self.createBucketListData = {
         name: ko.observable(),
         description: ko.observable()
@@ -28,7 +30,7 @@ var BucketListViewModel = function(createBucketListURL){
     self.createBucketListSuccess = function(resp){
         self.createBucketListModal.alertSuccess("BucketList Created!");
         self.createBucketListModal.stopLoadingAnimation();
-        window.location.href = siteURL + "signedin";
+        window.location.href = siteURL + "bucketlist";
     }
 
     self.createBucketListFailure = function(resp){
@@ -36,4 +38,28 @@ var BucketListViewModel = function(createBucketListURL){
         self.createBucketListModal.alertError(body.detail);
         self.createBucketListModal.stopLoadingAnimation();
     }
+
+    self.listBucketList = function(){
+        $.ajax({
+            url: createBucketListURL,
+            type: 'GET',
+            contentType: 'application/json',
+            headers: {Authorization: utils.authHeader()}
+        }).done(self.listBucketListSuccess).fail(self.listBucketListFailure);
+    }
+
+    self.listBucketListSuccess = function(resp){
+        console.log(resp);
+        self.bucketListItems = self.bucketListItems(resp);
+    }
+
+    self.listBucketListFailure = function(resp){
+        self.listBucketList();
+    }
+
+    self.splitDate = function(date){
+        return date.split('T')[0];
+    }
+
+    self.name = function(){return 'sss'}
 }

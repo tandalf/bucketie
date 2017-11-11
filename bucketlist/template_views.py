6c95aa3c11035e0cff24e9ea25004e3cbe0a6748
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2017-11-11 11:53:49 +0100

    added template view for the signed in page

--- bucketlist/template_views.py
+++ bucketlist/template_views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'bucketlist/index.html')
+
+def signedin(request):
+    return render(request, 'bucketlist/signedin.html')
